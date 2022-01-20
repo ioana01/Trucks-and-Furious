86e386c1b58c06f4f2a0ead7470179128d9144d1
Stefan Nedelcu <nedelcu.stefan17@yahoo.com>
2022-01-20 23:40:30 +0200

    Added Add-Truck-Form html
    TODO: handles, save in db, navbar button

--- trucks-and-furious/src/App.js
+++ trucks-and-furious/src/App.js
@@ -5,6 +5,7 @@ import Register from "./components/auth/register/register";
 import Login from "./components/auth/login/login";
 import Navbar from "./components/navbar/navbar";
 import DemandAndSupplyAddForm from "./components/forms/DemandAndSupplyAddForm/DemandAndSupplyAddForm";
+import AddTruckForm from "./components/forms/AddTruckForm/AddTruckForm";
 import PrivateRoute from "./components/private-route/private-route";
 import MapView from "./components/map/map";
 import DemandAndSupplyList from "./components/demand-and-supply-list/demand-and-supply-list";
@@ -20,6 +21,7 @@ function App() {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/add-truck" component={AddTruckForm}/>
               <PrivateRoute exact path="/demand-supply-add-form" component={DemandAndSupplyAddForm} />
               <PrivateRoute exact path="/" component={DemandAndSupplyList} />
               <PrivateRoute exact path="/item/:id" component={RequestDetails}/>
